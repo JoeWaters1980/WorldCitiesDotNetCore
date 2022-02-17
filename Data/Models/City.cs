ed20e1f8bb281f14fac2fdb5281189d5790a69dd
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-02-17 15:35:33 +0000

    database layout started context to do

---
+++ Data/Models/City.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        #region Constructor
+        public City()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        // <Summary>
+        // The unique id and primary Key foir this City
+        // </Summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        // <Summary>
+        // City name (in UTF8 format)
+        // </Summary>
+        public string Name { get; set; }
+        // <Summary>
+        // City name (in Name_ASCII format)
+        // </Summary>
+        public string Name_ASCII { get; set; }
+        // <Summary>
+        // City latitude 
+        // </Summary>
+        public decimal Lat { get; set; }
+        // <Summary>
+        // City Longitude 
+        // </Summary>
+        public string Lon { get; set; }
+        #endregion
+        // <Summary>
+        // Country Id (Foreign Key)
+        // </Summary>
+        [ForeignKey ("Country")]
+        public int CountryId { get; set; }
+
+        #region Navigation Properties
+        // <Summary>
+        // Country related to this city
+        // </Summary>
+        public virtual Country Country { get; set; }
+        #endregion
+    }
+}
---
+++ Data/Models/Country.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class Country
+    {
+        #region Constructor
+        public Country()
+        {
+
+        }
+        #endregion
+        #region Properties
+        // <Summary>
+        // The unique id and primary Key for this Country
+        // </Summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        // <Summary>
+        // Country Name (in UTF8 format)
+        // </Summary>
+        public string Name { get; set; }
+        // <Summary>
+        // Country code (in ISO 3166-1 ALPHA-2 format)
+        // </Summary>
+        public string ISO2 { get; set; }
+        // <Summary>
+        // Country code (in ISO 3166-1 ALPHA-3 format)
+        // </Summary>
+        public string ISO3 { get; set; }
+        #endregion
+        #region Navigation Properties
+        // <Summary>
+        // A list containing all the cities related to this country
+        // </Summary>
+        public virtual List<City> Cities { get; set; }
+        #endregion
+    }
+}
